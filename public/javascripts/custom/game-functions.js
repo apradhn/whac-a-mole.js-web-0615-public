8879a0c6dbc4a8bc104c4cfbb8916a4470e81ca1
apradhn <abhijit.pradhan6@gmail.com>
2015-07-20 13:53:43 -0400

    all done

--- public/javascripts/custom/game-functions.js
+++ public/javascripts/custom/game-functions.js
@@ -1,13 +1,29 @@
 function randomInt(min,max) {
   // create a range that includes min and max and return a random number from it
+  var rand = Math.floor(Math.random() * (max - min)) + min;
+  return rand;
 }
 
 function selectMole() {
-  // here you're going to find what mole is visible
-  // hide the visible mole
+  var visibleMole;
+  var visibleMoleId;
+  var newMoleId;
+  var newMole;
 
+  // here you're going to find what mole is visible
+  visibleMole = $('.mole').filter(':visible');
+  visibleMoleId = visibleMole.attr('id').match(/\d/)[0];
+
+  // hide the visible mole
+  visibleMole.hide('slide', {direction: 'down'});
   // select a new mole to visible, making sure it isn't the same as the previous one
+  newMoleId = randomInt(1, 9);
+  while (newMoleId === visibleMoleId) {
+    newMoleId = randomInt(1, 9);
+  }
   // and make the new mole visible
+  newMole = $('#mole-num-' + newMoleId);
+  newMole.show('slide', {direction: 'down'});
 }
 
 // the function below is called on game.js and calls on your selectMole() function every second
--- public/javascripts/custom/game.js
+++ public/javascripts/custom/game.js
@@ -3,8 +3,13 @@ $( document ).ready(function() {
 
   
   // create your click handler here
-
-
+  var moles = $('.mole.inv');
+  moles.each(function(i) {
+    var moleId = '#mole-num-' + (i + 1);
+    $(moleId).click(function() {
+      $(this).hide('slide', {direction: 'down'});
+    });
+  })
 
   // the below code calls on the play function found in game-functions.js
   // the play function calls on selectMole, then waits a second, then calls
